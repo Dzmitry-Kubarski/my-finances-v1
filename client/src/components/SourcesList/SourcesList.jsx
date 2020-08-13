436bc740ef98ad93a3c6b66515f0e0e42a8eb5c8
Дмитрий Кубарский <dzmitrykubarski@gmail.com>
2020-08-13 11:54:25 +0300

    Рабочий слайдер для счетов

--- client/src/components/SourcesList/SourcesList.jsx
+++ client/src/components/SourcesList/SourcesList.jsx
@@ -2,17 +2,57 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 
-//components
-import SourceItem from './SourceItem';
-
 //hooks
 import useSources from './useSources'
+
+//images
+import sourcesSvg from '../../images/money2.svg';
+import ellipsisHorizontalSvg from '../../images/ellipsis-horizontal.svg'
+
+
+// Swiper
+import SwiperCore, { Navigation, Pagination, Scrollbar, A11y } from 'swiper';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/swiper.scss';
+import 'swiper/components/navigation/navigation.scss';
+import 'swiper/components/pagination/pagination.scss';
+import 'swiper/components/scrollbar/scrollbar.scss';
+
+// install Swiper components
+SwiperCore.use([Navigation, Scrollbar, A11y]);
+
 
 const SourcesList = () => {
     const { data, isLoading, error } = useSources()
 
+    const sourcesJsx = data && data.map(({ title, total, _id }) => (
+        <SwiperSlide key={_id} className='sources-item'>
+            <div className='sources-item__row'>
+                <div className="logo  header__logo">
+                    <img className='logo__img' src={sourcesSvg} alt="logo" />
+                    <h1 className="logo__text">Finance</h1>
+                </div>
+
+
+                <button className='new-transaction__btn'>
+                    <img className='new-transaction__icon' src={ellipsisHorizontalSvg} alt='' />
+                </button>
+            </div>
+
+            <h4 className='sources-item__title'>{title}</h4>
+
+            <div className='sources-item__total'>{total} руб</div>
+        </SwiperSlide>
+    ))
+
     if (isLoading) return 'Loading...'
     if (error) return 'Ошибка при получении счетов: ' + error.message
+
+    if (!data) {
+        return (
+            <p>ничего нет</p>
+        )
+    }
 
     return (
         <div className='sources'>
@@ -21,9 +61,17 @@ const SourcesList = () => {
                 <NavLink className='btn-add' to="/create-source">+</NavLink>
             </div>
 
-            <ul className='sources__list'>
-                <SourceItem sources={data} />
-            </ul>
+            <Swiper
+                spaceBetween={50}
+                slidesPerView={1}
+                navigation
+                scrollbar={{ draggable: true }}
+                onSwiper={(swiper) => console.log(swiper)}
+                onSlideChange={() => console.log('slide change')}
+                className='slider'
+            >
+                {sourcesJsx}
+            </Swiper>
         </div>
     );
 };
