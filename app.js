a56bb5771c9efe0e11e60490e61a0b38b444fecf
Дмитрий Кубарский <dzmitrykubarski@gmail.com>
2020-08-20 17:39:44 +0300

    Build

--- app.js
+++ app.js
@@ -13,13 +13,13 @@ app.use('/api/sources', require('./routes/sources.routes'))
 app.use('/api/categories', require('./routes/categories.routes'))
 app.use('/t', require('./routes/redirect.routes'))
 
-// if (process.env.NODE_ENV === 'production') {
-//   app.use('/', express.static(path.join(__dirname, 'client', 'build')))
+if (process.env.NODE_ENV === 'production') {
+  app.use('/', express.static(path.join(__dirname, 'client', 'build')))
 
-//   app.get('*', (req, res) => {
-//     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-//   })
-// }
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  })
+}
 
 const PORT = config.get('port') || 5000
 
